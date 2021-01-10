685fc38d3d6fc1a3006695ea87d6d0f309f4bd2b
0909oje <0909oje@naver.com>
2021-01-10 21:35:34 +0900

    input[][] 통해 각 열에 바이트 수, 비용 넣기

--- ICPC_winter_2021/src/동적계획법/ex7579.java
+++ ICPC_winter_2021/src/동적계획법/ex7579.java
@@ -7,12 +7,12 @@ public class ex7579 {
 		Scanner sc = new Scanner(System.in);
 		
 		int N =  sc.nextInt(), M = sc.nextInt();
-		int[][] input = new int[N+1][2]; //첫번째 열 바이트수, 두번째 열 비활성화 비용
-		int[][] dp = new int[N+1][10000001]; // dp[바이트][비용]
+		int[][] input = new int[N][2]; //첫번째 열 바이트수, 두번째 열 비활성화 비용
+		int[][] dp = new int[N][100001]; // dp[바이트][비용]
 		int min=Integer.MAX_VALUE; // 최소값을 정수의 최대값으로 설정해놓는다.
 		
-		for(int i=1;i<=N;i++) input[i][0]=sc.nextInt(); // 바이트 수
-		for(int i=1;i<=N;i++) input[i][1]=sc.nextInt(); // 비용
+		for(int i=0;i<N;i++) input[i][0]=sc.nextInt(); // 바이트 수
+		for(int i=0;i<N;i++) input[i][1]=sc.nextInt(); // 비용
 		
 		for(int i=1;i<=N;i++) {
 			int m = input[i][0], val=input[i][1];
