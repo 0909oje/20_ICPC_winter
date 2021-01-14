7e158c6d8d573224c61a7f6405825922a7390def
0909oje <0909oje@naver.com>
2021-01-14 22:49:12 +0900

    한번에 커밋

--- ICPC_winter_2021/src/동적계획법/ex1949.java
+++ ICPC_winter_2021/src/동적계획법/ex1949.java
@@ -18,7 +18,7 @@ package 동적계획법;
 			visit = new boolean[N+1];
 			
 			for (int i = 1; i <= N; ++i) {
-				citizen[i] = sc.nextInt();
+				citizen[i] = sc.nextInt(); 
 				edgeList[i] = new ArrayList<>();
 			}
 			
@@ -32,11 +32,11 @@ package 동적계획법;
 			
 			dp = new int[N+1][2];
 			/* 
-			    dp[i][0] = i번째 마을이 우수 마을일때 우수 마을의 주민 수 Max
-			             => 조건에 따라 i+1번째 마을은 우수 마을이 될수가 없음
+			    dp[i][0] = i번째 마을이 우수 마을O -> 우수 마을의 주민 수 Max
+			             =>  i+1번째 마을은 우수 마을 x
 			              
-			    dp[i][1] = i번째 마을이 우수마을이 아닐때 우수 마을의 주민 수 Max
-			             => 조건에 따라 i+1번째 마을은 우수 마을이거나 아니거나 둘다 가능 
+			    dp[i][1] = i번째 마을이 우수마을X -> 우수 마을의 주민 수 Max
+			             =>  i+1번째 마을은 우수 마을 O or X
 			*/
 			
 			visit[1] = true;
@@ -44,19 +44,18 @@ package 동적계획법;
 			System.out.println(Math.max(dp[1][0], dp[1][1]));
 		}
 		
-		public static void getMaxCitizen(int v) {
-			for (int i = 0; i < edgeList[v].size(); ++i) {
-				int nv = edgeList[v].get(i);
+		public static void getMaxCitizen(int v) { //DFS
+			for (int i = 0; i < edgeList[v].size(); ++i) { // 리스트 사이즈를 계속 탐색
+				int nv = edgeList[v].get(i); //리스트에서 원소 꺼내옴
 				
-				if (visit[nv]) continue;
-				visit[nv] = true;
+				if (visit[nv]) continue; // 방문한 적 있으면 넘기고
+				visit[nv] = true; // 방문한 적 없으면 계속하기
 
-				getMaxCitizen(nv);
+				getMaxCitizen(nv); //다음 노드 가서 탐색
 				
 				dp[v][0] += dp[nv][1];
 				dp[v][1] += Math.max(dp[nv][0], dp[nv][1]);
 			}
-			
 	        //v번째 마을을 우수 마을로 선정할 경우 시민의 수를 더함
 			dp[v][0] += citizen[v];
 		}
--- ICPC_winter_2021/src/동적계획법/ex2248.java
+++ ICPC_winter_2021/src/동적계획법/ex2248.java
@@ -13,7 +13,7 @@ public class ex2248 {
 
 	        dp = new long[n + 1][l + 1];
 	        dp[0][0] = 1; // 후에 for문에서 계산용으로 1설정
-
+	        
 	        for (int y = 1; y <= n; y++) {
 	            dp[y][0] = 1; // 이진수가 y개의 자리를 가지고 있고 1이 0개 있다면 경우의 수 1
 	            for (int x = 1; x <= l; x++) { // 이 때 y개의 자리에 x개의 1을 가지고 있다면
@@ -22,14 +22,15 @@ public class ex2248 {
 	                // y-1개의 자리에 x개의 1을 가진 수에 0 추가를 더한다.
 	            }
 	        }
-
+	        
 	        StringBuilder result = new StringBuilder();  
 	        // 스트링 값을 계속 바꿀 수 있게 StringBuilder 사용
 	        for (int pos = n; pos > 0; pos--) { // n부터 1번째 자리 탐색
 	            final int tempPos = pos; 
 	            long count = IntStream.range(0, l + 1). // 카운트번위를 현재 남은 1bit 수로 설정
-	            		mapToLong(k -> dp[tempPos - 1][k]).
-	                    sum(); // count요소 총합 반환
+	            		mapToLong(k -> dp[tempPos - 1][k]). //여기서부터
+	                    sum(); //여기까지 뭔소리여,, 아마 이진수단위로 탐색할 때
+	            //다음 탐색 범위 구할 때 쓰는 듯
 	            int digit;
 	            if (count < i) { //count가 주어진 i보다 작으면,
 	                digit = 1; // i번째 이진수는 다음 비트에 1이 올 것임 (count보다 크면 1, 작으면 0붙음)
